97a2c065d290cbeac2053f974c7ac524d46eb4a5
mohamed salad <abdikariimsalad55@gmail.com>
2026-04-06 11:58:33 +0300

    completed exercise_2

---
+++ exercise_2/index.ts
@@ -0,0 +1,9 @@
+let  names : string[] = ['cali ', 'rayaan', 'shamso']
+let grades: number[] = [75 , 80 , 65]
+let Status : boolean[] =  [ false , true , ]
+
+
+// 2 can only applly string
+
+let products : string [] = ['cali', 'xaawo' ]
+let  locations : [string , number , number] = ['mugdishu', 252 , 61911828282]
---
+++ exercise_2/readme.md
@@ -0,0 +1,5 @@
+#  exercise_2 typescript
+
+1. variables are  names , grades ,   status  in form typescript code 
+2. making products array only apply string value
+3. making varible location apply tuples  [string , number number]
